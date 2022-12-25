8c90d249fae686aaaad18a066070a22a1b4d99f4
Airlangga Fidiyanto <airlangga.rasyad@live.com>
2022-12-25 16:52:40 +0700

    hotfix add education section

--- src/App.js
+++ src/App.js
@@ -50,6 +50,13 @@ const ProjectSection = () => {
             </div>
           ))}
       </div>
+    </div>
+  );
+};
+
+const TechStackSection = () => {
+  return (
+    <div>
       <div>
         <h2>
           <span style={{ paddingLeft: 0 }}>🖥️ Tech Stacks</span>
@@ -60,6 +67,38 @@ const ProjectSection = () => {
               <img style={{ width: "5em" }} src={stack.imageURL} />
             </div>
           ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const EducationSection = () => {
+  return (
+    <div>
+      <div>
+        <h2>
+          <span style={{ paddingLeft: 0 }}>🎓 Education</span>
+        </h2>
+      </div>
+      <div
+        className="education-container"
+      >
+        <div className="bachelor education">
+          <div><img style={{width:"8vw", margin:"0 1rem"}} src={"https://agricia.faperta.ugm.ac.id/wp-content/uploads/sites/377/2018/06/logo-ugm-png.png"}/></div>
+          <div>
+            <h3>Universitas Gadjah Mada <br/></h3>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+            <h4 style={{margin:0}}><i>BEng (S.T.) in Electrical Engineering</i></h4>
+            <h4 style={{margin:0}}>August 2019 -- August 2023</h4>
+            </div>
+            <ul style={{margin:"0", padding: "1em"}}>
+              <li><b>Thesis:</b> Firmware design for Trans Gadjah Mada GPS Tracker using STM-32 Platform</li>
+              <li>Supervisor: I Wayan Mustika, S.T., M.Eng., Ph.D. and Ir. Agus Bejo, ST, M.Eng., D.Eng., IPM.</li>
+              <li>Signal and System labworks assistant with Dr. Dyonisius Dony Ariananda</li>
+              <li>Minor in Computer Engineering</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -84,6 +123,8 @@ function App() {
     <>
       <TitleSection />
       <ProjectSection />
+      <EducationSection />
+      <TechStackSection />
       <SocialSection />
     </>
   );
